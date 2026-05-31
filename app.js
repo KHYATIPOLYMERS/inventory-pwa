5b89a420d0e840643f4de13cbe26a8f21403abce
KHYATIPOLYMERS <khyatipolymers@gmail.com>
2026-05-31 15:27:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,20 +5,6 @@ if ("serviceWorker" in navigator) {
 }
 
 function openView(module) {
-  if (module === "products") {
-    window.location.href = "products.html"; // go to Products module page
-  }
-  else if (module === "customers") {
-    window.location.href = "customers.html";
-  }
-  else if (module === "quotations") {
-    window.location.href = "quotations.html";
-  }
-  else if (module === "gallery") {
-    window.location.href = "gallery.html";
-  }
-  else if (module === "downloads") {
-    window.location.href = "downloads.html";
-  }
+    window.location.href = `modules/${module}/${module}.html`;
 }
 
